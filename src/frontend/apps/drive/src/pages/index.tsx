9408f7d457b1c2fb614d05c0c4dbf60d1fb31249
alevale <alexvales111@gmail.com>
2025-09-12 23:42:49 +0200

    impr: Update logos and app names with mosa branding

--- src/frontend/apps/drive/src/pages/index.tsx
+++ src/frontend/apps/drive/src/pages/index.tsx
@@ -147,7 +147,7 @@ const HomePageContent = () => {
           }
         />
       </HomeGutter>
-      <Footer {...footerCustommization} />
+      {false && <Footer {...footerCustommization} />}
     </HomePageLayout>
   );
 }

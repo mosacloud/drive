34620df7065ef8a43e9a25e9121b0e52c9e3432c
alevale <alexvales111@gmail.com>
2025-09-12 23:42:49 +0200

    impr: Update logos with mosa branding

--- src/frontend/apps/drive/src/pages/index.tsx
+++ src/frontend/apps/drive/src/pages/index.tsx
@@ -109,7 +109,7 @@ export default function HomePage() {
           }
         />
       </HomeGutter>
-      <Footer {...footerCustommization} />
+      {false && <Footer {...footerCustommization} />}
     </>
   );
 }

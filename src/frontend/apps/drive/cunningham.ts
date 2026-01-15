952bfcce35671bafdf6d5b1d5c7d4596ea4038ef
Niels Kersic <niels@zzapps.nl>
2026-01-15 14:41:28 +0100

    feat: add mosa.cloud login page

--- src/frontend/apps/drive/cunningham.ts
+++ src/frontend/apps/drive/cunningham.ts
@@ -59,6 +59,11 @@ const themesImages = {
     logo: "/assets/logo_beta.svg",
     "logo-icon": "/assets/logo-icon_beta.svg",
   },
+  mosa: {
+    favicon: "/assets/favicon-light.png",
+    logo: "/assets/mosa-horizontal.svg",
+    "logo-icon": "/assets/mosa.svg",
+  },
 };
 
 const themesGaufre = {
@@ -74,6 +79,10 @@ const themesGaufre = {
   default: {
     widgetPath: "https://static.suite.anct.gouv.fr/widgets/lagaufre.js",
     apiUrl: "https://lasuite.numerique.gouv.fr/api/services",
+  },
+  mosa: {
+    widgetPath: "",
+    apiUrl: "",
   },
 };
 
@@ -402,6 +411,45 @@ const defaultConfig = deepMerge(cunninghamConfig, {
     },
     default: {
       components: getComponents("default"),
+    },
+    mosa: {
+      globals: {
+        colors: {
+          "brand-050": "#E7EDFE",
+          "brand-100": "#CFDCFD",
+          "brand-150": "#B7CAFC",
+          "brand-200": "#9FB9FB",
+          "brand-250": "#87A7FA",
+          "brand-300": "#6F96F9",
+          "brand-350": "#5784F8",
+          "brand-400": "#3F73F7",
+          "brand-450": "#2761F6",
+          "brand-500": "#0F50F5",
+          "brand-550": "#0443F2",
+          "brand-600": "#033BD9",
+          "brand-650": "#0334C0",
+          "brand-700": "#022CA7",
+          "brand-750": "#02258E",
+          "brand-800": "#011D75",
+          "brand-850": "#01165C",
+          "brand-900": "#010E43",
+          "brand-950": "#00072A",
+          "logo-1": "#0443F2",
+        },
+        font: {
+          families: {
+            base: "Inter, Roboto Flex Variable, sans-serif",
+            accent: "Inter, Roboto Flex Variable, sans-serif",
+          },
+        },
+      },
+      components: {
+        ...getComponents("mosa"),
+        "la-gaufre": false,
+        "home-proconnect": false,
+        beta: false,
+        footer: false,
+      },
     },
   },
 });

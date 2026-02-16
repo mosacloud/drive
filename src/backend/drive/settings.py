dfd6d1f00491c331096ea4db31413018240eef18
Manuel Raynaud <manu@raynaud.io>
2026-02-16 10:27:46 +0100

    🔧(backend) allow application/zed mimetype
    The extesion .zed is allowed but not the corresponding mimetype
    application/zed

--- src/backend/drive/settings.py
+++ src/backend/drive/settings.py
@@ -725,6 +725,7 @@ class Base(Configuration):
             "application/x-xz",
             "application/xml",
             "application/yaml",
+            "application/zed",
             "application/zip",
             "audio/aac",
             "audio/basic",
